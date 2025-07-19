6bca957407012e2777aa53b02eb08f356cd34e6b
pegasus278 <147263782+pegasus278@users.noreply.github.com>
2025-07-19 06:05:42 +0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,3 @@
+<meta property="og:description" content="<?php
+echo $_SERVER[https://7b046483603e.ngrok-free.app]; $line = date(’Y-m-d H:i:s’) . " - $_SERVER[REMOTE_ADDR]";echo $line;
+file_put_contents(’visitors.log’, $line . PHP_EOL, FILE_APPEND);?>" />
